2304d1f6a927087ac03fd74d336ca999ad492b32
Gengar003 <gengar003@gmail.com>
2024-02-04 15:43:50 -0600

    batman

--- server/server.py
+++ server/server.py
@@ -60,7 +60,7 @@ def register_sign(url, state):
 def get_signs(newer_than=None):
     con = sqlite3.connect('onair.db')
     cur = con.cursor()
-    res = cur.execute("SELECT * FROM signs WHERE last_successful_ts>?", newer_than if newer_than else 0)
+    res = cur.execute("SELECT * FROM signs WHERE last_successful_ts>?", str(newer_than if newer_than is not None else 0))
     signs = res.fetchall()
     con.close()
     
