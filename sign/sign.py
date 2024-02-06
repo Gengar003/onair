9c4a2b25d9b4ba7395430516c2188e93e59f20e4
Gengar003 <gengar003@gmail.com>
2024-02-06 08:47:37 -0600

    only compute callback host if we need

--- sign/sign.py
+++ sign/sign.py
@@ -13,22 +13,29 @@ API_BASE = "/onair/api"
 API_VERSION = "v1"
 API_URL = f"{API_BASE}/{API_VERSION}"
 
-hostname = socket.getfqdn()
-ip = socket.gethostbyname_ex(hostname)[2][1]
-
 parser = argparse.ArgumentParser(
     prog='sign.py',
     description="A sign that can be toggled on or off"
 )
 parser.add_argument('-s', '--server', type=str, help='The full server endpoint URL to register with for push updates.')
 parser.add_argument('-p', '--port', type=int, default=5000, help='The port to listen on')
-parser.add_argument('-h', '--host', type=str, default=ip, help="The host or IP to register with the server for push  updates, if it isn't just our IP + port")
+parser.add_argument('-h', '--host', type=str, help="The host or IP to register with the server for push  updates, if it isn't just our IP + port")
 parser.add_argument('-c', '--command', nargs='+', type=str, help="Command to execute when toggled. %STATUS%, if present, will be replaced with `true' or `false'.")
 parser.add_argument('-i', '--idempotent', action='store_true', help="If it is safe to call the --command on every state update. If false (default), commands only run when state CHANGES according to the sign's own memory.")
 args = parser.parse_args()
 
 app = Flask(__name__)
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('192.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 # register this sign with a server
 def register(server, host, port):
@@ -86,5 +93,7 @@ def set_state():
 
 if __name__ == '__main__':
     if args.server:
-        register(args.server, args.host, args.port)
+        if not args.host:
+            host = get_local_ip()
+        register(args.server, args.host if args.host else host, args.port)
     app.run(debug=True, host="0.0.0.0", port=args.port)
