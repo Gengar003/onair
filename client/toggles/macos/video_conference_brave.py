6caac3d76602e090b1b98ef425c7478c5fc6ef90
Gengar003 <gengar003@gmail.com>
2024-03-29 17:24:13 -0500

    renamed google meet detection refactored zoom detection to be more generic

--- client/toggles/macos/video_conference_brave.py
+++
@@ -1,5 +0,0 @@
-from toggles.macos.lib import video_conference_chromium
-
-def run_and_call(callback, poll_interval_s=5, start_threshold=5, cpu_threshold=15):
-    video_conference_chromium.run_and_call(callback, "Brave Browser", poll_interval_s, start_threshold, cpu_threshold)
-
