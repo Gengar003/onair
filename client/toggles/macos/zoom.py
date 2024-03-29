9165d528f59760fe6ac2097f5cd97d55d86f2f9a
Gengar003 <gengar003@gmail.com>
2024-03-29 17:32:52 -0500

    fix import path

--- client/toggles/macos/zoom.py
+++ client/toggles/macos/zoom.py
@@ -1,4 +1,4 @@
-from client.toggles.macos.lib import app_cpu_with_headset
+from toggles.macos.lib import app_cpu_with_headset
 
 def run_and_call(callback, poll_interval_s=5, start_threshold=5, cpu_threshold=15):
     app_cpu_with_headset.run_and_call(callback, "zoom.us.app", poll_interval_s, start_threshold, cpu_threshold)
