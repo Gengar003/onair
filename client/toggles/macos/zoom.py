230739615d2a060bb8d657df78f937736c6e5248
Gengar003 <gengar003@gmail.com>
2024-03-29 17:40:14 -0500

    1s poll interval for zoom

--- client/toggles/macos/zoom.py
+++ client/toggles/macos/zoom.py
@@ -1,5 +1,5 @@
 from toggles.macos.lib import app_cpu_with_headset
 
-def run_and_call(callback, poll_interval_s=5, start_threshold=3, cpu_threshold=15):
+def run_and_call(callback, poll_interval_s=1, start_threshold=5, cpu_threshold=15):
     app_cpu_with_headset.run_and_call(callback, "zoom.us.app", poll_interval_s, start_threshold, cpu_threshold)
 
